97df24fcb3f2bc2f8a7d18236b5f5409d609ada3
stevejs <stevenson.sharp@gmail.com>
2018-01-28 22:41:02 -0800

    Adjust account confirmed login process

--- src/App.js
+++ src/App.js
@@ -101,6 +101,24 @@ class About extends Component {
 }
 
 class AccountConfirmedScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: ""
+        };
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    handleEmailChange(email) {
+        this.setState({email: email})
+    }
+
+    handlePasswordChange(password) {
+        this.setState({password:password})
+    }
+
     render() {
         return (
             <main role="main" class="container">
@@ -109,10 +127,43 @@ class AccountConfirmedScreen extends Component {
                     <form class="form-signin" method="POST" action="/login">
                         <h2 class="form-signin-heading">Please log in</h2>
                         <label for="email" class="sr-only">Email address</label>
-                        <input type="email" name="email" class="form-control" placeholder="Email address" required autofocus />
+                        <input
+                            type="email"
+                            name="email"
+                            className="form-control"
+                            placeholder="Email address"
+                            onChange={(e) => this.handleEmailChange(e.target.value)}
+                            required autofocus />
                         <label for="password" class="sr-only">Password</label>
-                        <input type="password" name="password" class="form-control" placeholder="Password" required />
-                        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                        <input
+                            type="password"
+                            name="password"
+                            className="form-control"
+                            placeholder="Password"
+                            onChange={(e) => this.handlePasswordChange(e.target.value)}
+                            required />
+                        <button class="btn btn-lg btn-primary btn-block"
+                            type="button"
+                            onClick={(e) => {
+                                var that = this;
+                                var formData = new FormData();
+                                formData.append("email", that.state.email);
+                                formData.append("password", that.state.password);
+                                var resetsXHR = new XMLHttpRequest();
+                                resetsXHR.addEventListener('load', function(event) {
+                                    if (event.target.status === 200) {
+                                        window.location.href = "/messages";
+                                    } else {
+                                        // TODO: alert user login failed
+                                    }
+                                });
+                                resetsXHR.addEventListener('error', function(event) {
+                                    // TODO: alert user login failed
+                                });
+                                resetsXHR.open('POST', '/login');
+                                resetsXHR.send(formData);
+                            }}
+                            >Sign in</button>
                     </form>
                 </div>
             </main>
@@ -1819,8 +1870,6 @@ class LoginForm extends Component {
             type="button"
             onClick={(e) => {
                 var that = this;
-                console.log(that.state.email);
-                console.log(that.state.password);
                 var formData = new FormData();
                 formData.append("email", that.state.email);
                 formData.append("password", that.state.password);
