cd0c0005373ba2895a2dd9de6d980143d8914ff2
stevejs <stevenson.sharp@gmail.com>
2018-01-28 22:23:20 -0800

    Modify login to be xhr-based, rather than form-based

--- src/App.js
+++ src/App.js
@@ -1777,15 +1777,34 @@ class LogoutForm extends Component {
 }
 
 class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: ""
+        };
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    handleEmailChange(email) {
+        this.setState({email: email})
+    }
+
+    handlePasswordChange(password) {
+        this.setState({password:password})
+    }
+
   render() {
     return (
-      <form className="form-inline my-2 my-lg-0" method="POST" action="/login">
+      <form className="form-inline my-2 my-lg-0" >
         <input
           className="form-control mr-sm-2"
           type="email"
           name="email"
           placeholder="Email"
           aria-label="Email"
+          onChange={(e) => this.handleEmailChange(e.target.value)}
         />
         <input
           className="form-control mr-sm-2"
@@ -1793,8 +1812,32 @@ class LoginForm extends Component {
           name="password"
           placeholder="Password"
           aria-label="Password"
+          onChange={(e) => this.handlePasswordChange(e.target.value)}
         />
-        <button className="btn btn-primary" type="submit">
+        <button
+            className="btn btn-primary"
+            type="button"
+            onClick={(e) => {
+                var that = this;
+                console.log(that.state.email);
+                console.log(that.state.password);
+                var formData = new FormData();
+                formData.append("email", that.state.email);
+                formData.append("password", that.state.password);
+                var resetsXHR = new XMLHttpRequest();
+                resetsXHR.addEventListener('load', function(event) {
+                    if (event.target.status === 200) {
+                        window.location.href = "/messages";
+                    } else {
+                        // TODO: alert user login failed
+                    }
+                });
+                resetsXHR.addEventListener('error', function(event) {
+                    // TODO: alert user login failed
+                });
+                resetsXHR.open('POST', '/login');
+                resetsXHR.send(formData);
+            }}>
           Login
         </button>
       </form>
